ba3c0b3ba610f55a5fe5a4bb5c909f672da7b850
joana sousa <75800825+Joanadekastro@users.noreply.github.com>
2022-02-03 22:29:09 +0000

    commit 3

---
+++ exercise_3.js
@@ -0,0 +1,15 @@
+/*Listar na consola os números de 1 a 10, mas pela ordem inversa - utilizar um ciclo for ou while:
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1*/
+
+for (let i = 10; i > 0; i--) {
+  console.log(i);
+}
